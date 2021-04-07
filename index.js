5eba535db928c24eba40a53fdffac23f337a7345
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-07 12:15:39 +0600

    add html and javascript for the project

---
+++ index.js
@@ -0,0 +1,17 @@
+for(var x=1;x<=100;x++){
+    if(x==10){
+        continue;
+       }
+    
+    document.write(" "+x);
+   
+}
+for(var x=1;x<=100;x++){
+    
+    document.write(" "+x);
+
+    if(x==0){
+        continue
+    }
+}
+
